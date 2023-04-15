5ce0442e3e2c2bb8ae3325c058a05888f11369e9
Jbnado <73846881+Jbnado@users.noreply.github.com>
2023-04-14 21:44:55 -0300

    Create Main.java

---
+++ Heranca/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Aluno aluno = new Aluno(1, "JB", "teste");
+        aluno.mostrar();
+        aluno.excluir();
+        aluno.mostrar();
+    }
+}
