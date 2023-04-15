13cc3a8eaaef030ac2d0e6095b5bd3bdab177a8a
Jbnado <73846881+Jbnado@users.noreply.github.com>
2023-04-14 21:46:27 -0300

    Update Pessoa.java

--- Heranca/Pessoa.java
+++ Heranca/Pessoa.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package heranca;
-
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author 2840482113042
- */
 public class Pessoa {
     protected String nome, endereco;
 
